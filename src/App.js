ac19059a18d309027029511273fba3946d99ae6b
Nate D <nate@idontchop.com>
2022-04-14 21:16:53 -0700

    Added presentation module

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import GuestBook from './GuestBook.js';
 import './App.css';
+import './style.css'
 import 'bootstrap/dist/css/bootstrap.css';
 import {Welcome} from './Components/Welcome'
 import {Presentation, ConfigurableSlide} from 'react-scroll-presentation'
@@ -10,19 +11,18 @@ function App() {
   return (
   <div>
     <Presentation>
-      <ConfigurableSlide>
-        {/*<Welcome />*/}
-        <div></div>
+      <ConfigurableSlide slideIn>
+        <Welcome />
       </ConfigurableSlide>
     </Presentation>
 
-    <div class="pageSection2">
-      <div class="sectionHeader container dateTheme">
-        <div class="sectionHeaderH2Wrapper">
+    <div className="pageSection2">
+      <div className="sectionHeader container dateTheme">
+        <div className="sectionHeaderH2Wrapper">
           <h2><a name="GuestBook"></a>Guest Book</h2>
         </div>
       </div>
-      <div class="sectionBody">
+      <div className="sectionBody">
           
 
         <p>Please take a momemnt to sign my <b>Guest Book</b>. Even if I don't fit the position you are filling, by leaving your
--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -1,7 +1,7 @@
 import React from 'react'
 export const Welcome = () => {
 
-return <div className="pageSection2">
+return <div className="pageSection2" style={{height: "100vh"}}>
 <div className="sectionHeader container dateTheme">
   <div className="sectionHeaderH2Wrapper">
     <h2>Welcome!</h2>
@@ -10,14 +10,14 @@ return <div className="pageSection2">
 <div className="sectionBody">
   <div className="container-fluid">
     <div className="row">
-      <div className="col-md-6">
+      {/*<div className="col-md-6">
         <div className="videoContainer">
           <video width="100%" height="100%" controls> 
             <source src="https://idontchop.com/videos/opening.mp4" type="video/mp4" />
             Video Alt Display </video>
         </div>
-      </div>
-      <div className="col-md-6"> <h3>Let's get to work!</h3>
+      </div>*/}
+      <div className="col-md-6"> 
         <p>Hello, my name is <b>Nathan Dunn</b>, I'm a Software Developer from Las Vegas, Nevada.</p>
         <p>My experience on web apps ranges from the browser (React) to the bare metal Linux machine
           running Docker, and everything in between.</p>
